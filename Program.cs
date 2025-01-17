d33e545db604c102daba23abe56b47261d2b2c1d
mt-ib <141046447+mt-ib@users.noreply.github.com>
2025-01-17 15:06:12 +0100

    enum-int (default)

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using LinqToDB.Data;
 using LinqToDB.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Npgsql;
 
 await using var ctx = new AppDbContext();
 await ctx.Database.EnsureDeletedAsync();
@@ -12,7 +10,6 @@ var entities = new List<Foo>
 {
     new(0, "EF", FooStatus.Open),
     new(0, "EF", FooStatus.Closed),
-    // new(0, "EF", null),
 };
 
 ctx.Foos.AddRange(entities);
@@ -22,11 +19,6 @@ await ctx.BulkCopyAsync(
     new BulkCopyOptions(),
     entities.Select(x => x with { Source = "linq2db" })
 );
-
-var foos = await ctx.Foos.ToListAsync();
-
-foreach (var foo in foos)
-    Console.WriteLine(foo);
 
 
 public class AppDbContext : DbContext
@@ -38,29 +30,7 @@ public class AppDbContext : DbContext
         const string connectionString =
             "Host=localhost;Port=5432;Database=linq2db;Username=postgres;Password=example;Include Error Detail=true";
 
-        // default, string #1 & #2
         optionsBuilder.UseNpgsql(connectionString);
-
-        // enum #1
-        // optionsBuilder.UseNpgsql(connectionString, options => options.MapEnum<FooStatus>("foo_status"));
-
-        // enum #2 & #3
-        // var dataSource = new NpgsqlDataSourceBuilder(connectionString)
-        //     .MapEnum<FooStatus>()
-        //     .Build();
-        // optionsBuilder.UseNpgsql(dataSource, options => options.MapEnum<FooStatus>());
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Foo>()
-            .Property(x => x.Status)
-            // .HasConversion<string>() // string #1
-            // .HasConversion(new EnumToStringConverter<FooStatus>()) // string #2
-            ;
-
-        // enum#3
-        // modelBuilder.HasPostgresEnum<FooStatus>();
     }
 }
 
